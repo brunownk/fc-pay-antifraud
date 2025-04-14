d737d3c588664cf5fee6d148642b7ed26adb4723
Bruno Neckel <neckel.bw@gmail.com>
2025-04-14 01:39:53 -0300

    feat: update docker-compose and invoice consumer

--- src/invoices/invoices.consumer.ts
+++ src/invoices/invoices.consumer.ts
@@ -1,21 +1,27 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { FindAllInvoiceDto } from './dto/find-all-invoice.dto';
-import { InvoicesService } from './invoices.service';
+import { Controller, Logger } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { FraudService } from './fraud/fraud.service';
 
-@Controller('invoices')
+export type PendingInvoicesMessage = {
+  account_id: string;
+  amount: number;
+  invoice_id: string;
+};
+
+@Controller() //injeções
 export class InvoicesConsumer {
-  constructor(private readonly invoicesService: InvoicesService) {}
+  private logger = new Logger(InvoicesConsumer.name);
 
-  @Get()
-  findAll(@Query() filter: FindAllInvoiceDto) {
-    return this.invoicesService.findAll({
-      withFraud: filter.with_fraud,
-      accountId: filter.account_id,
+  constructor(private fraudService: FraudService) {}
+
+  @EventPattern('pending_transactions')
+  async handlePendingInvoices(@Payload() message: PendingInvoicesMessage) {
+    this.logger.log(`Processing invoice: ${message.invoice_id}`);
+    await this.fraudService.processInvoice({
+      account_id: message.account_id,
+      amount: message.amount,
+      invoice_id: message.invoice_id,
     });
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.invoicesService.findOne(id);
+    this.logger.log(`Invoice processed: ${message.invoice_id}`);
   }
 }
